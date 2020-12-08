141923ec1724bb96ae33f4a4b9bb61a4d00bba46
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-12-08 20:31:00 +0000

    Create run1.py

---
+++ ADV-NET/Filtering/Lesson1/run1.py
@@ -0,0 +1,20 @@
+"""
+INSTALL THE FOLLOWING:
+
+pip3 install nornir
+pip3 install nornir-scrapli
+pip3 install nornir_utils
+"""
+
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_configs_from_file
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+def banner_push(task):
+    task.run(task=send_configs_from_file, name="Configuring Banner", file="reader.txt")
+
+north_targets = nr.filter(state="michigan")
+results = north_targets.run(task=banner_push)
+print_result(results)
