9c6be16f5decb05160ba118172c810085f77c572
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-12-22 14:31:07 +0000

    Create runbook3.py

---
+++ ADV-NET/Backups/runbook3.py
@@ -0,0 +1,39 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command
+from nornir_utils.plugins.functions import print_result
+from nornir_utils.plugins.tasks.files import write_file
+from datetime import date
+import pathlib
+import os
+
+my_list = []
+user_input = input("Enter commands: ")
+while user_input:
+    my_list.append(user_input)
+    user_input = input("Enter commands: ")
+    if user_input == "\n":
+        break
+
+
+def backup_configurations(task):
+    for cmd in my_list:
+        name = str(cmd)
+        folder = name.replace(" ", "-")
+        folder = folder.strip("\n")
+        config_dir = "config-archive"
+        date_dir = config_dir + "/" + str(date.today())
+        command_dir = date_dir + "/" + folder
+        pathlib.Path(config_dir).mkdir(exist_ok=True)
+        pathlib.Path(date_dir).mkdir(exist_ok=True)
+        pathlib.Path(command_dir).mkdir(exist_ok=True)
+        r = task.run(task=send_command, command=cmd)
+        task.run(task=write_file,content=r.result,filename=f"" + str(command_dir) + "/" + task.host.name + ".txt")
+
+nr = InitNornir(config_file="config.yaml")
+
+result = nr.run(name="Creating Backup Archive", task=backup_configurations)
+if result.failed:
+    print("FAILED!")
+    result.raise_on_error()
+else:
+    print("ARCHIVE SUCCESSFUL")
