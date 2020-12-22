5d70322664617d39a10052ee9c88b7b40c1e1c8a
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-12-22 14:32:46 +0000

    Update runbook1.py

--- ADV-NET/Backups/runbook1.py
+++ ADV-NET/Backups/runbook1.py
@@ -4,13 +4,14 @@ from nornir_utils.plugins.functions import print_result
 from nornir_utils.plugins.tasks.files import write_file
 
 
+nr = InitNornir(config_file="config.yaml")
+
 def backup_configurations(task):
     cmds = ["show run", "show version"]
     for cmd in cmds:
         r = task.run(task=send_command, command=cmd)
         task.run(task=write_file,content=r.result,filename=f"{cmd}-{task.host}.txt")
 
-nr = InitNornir(config_file="config.yaml")
 
 results = nr.run(name="Creating Backup Archive", task=backup_configurations)
 print_result(results)
