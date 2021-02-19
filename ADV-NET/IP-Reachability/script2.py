7f4412115f5cd263d42b4530c99a533c6db5ec20
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-02-19 22:37:32 +0000

    Create script2.py

---
+++ ADV-NET/IP-Reachability/script2.py
@@ -0,0 +1,39 @@
+"""
+Author: IPvZero
+"""
+
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command
+from nornir_utils.plugins.functions import print_result
+from rich import print as rprint
+
+nr = InitNornir(config_file="config.yaml")
+
+target_list = []
+failed_list = []
+
+def get_ip(task):
+    result = task.run(task=send_command, command="show ip interface brief")
+    task.host["facts"] = result.scrapli_response.genie_parse_output()
+    interfaces = task.host["facts"]["interface"]
+    for intf in interfaces:
+        if intf.startswith("Loop"):
+            ip_addr = interfaces[intf]["ip_address"]
+            target_list.append(ip_addr)
+
+def ping_test(task):
+    for ip_address in sorted_list:
+        result = task.run(task=send_command, command="ping " + ip_address)
+        response = result.result
+        if not "!!!" in response:
+            failed_list.append(f"{task.host} cannot ping {ip_address}")
+
+
+nr.run(task=get_ip)
+sorted_list = sorted(target_list)
+nr.run(task=ping_test)
+if failed_list:
+    sorted_fails = sorted(failed_list)
+    rprint(sorted_fails)
+else:
+    rprint("[green]All pings were successful[/green]")
