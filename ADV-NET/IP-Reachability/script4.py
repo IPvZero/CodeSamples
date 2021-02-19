c351086a0c47d7d05342f21a31d463e15101162d
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-02-19 22:41:03 +0000

    Update script4.py

--- ADV-NET/IP-Reachability/script4.py
+++ ADV-NET/IP-Reachability/script4.py
@@ -10,29 +10,25 @@ from rich.table import Table
 CLEAR = "clear"
 os.system(CLEAR)
 localtime = time.asctime(time.localtime(time.time()))
-active_list = []
-inactive_list = []
-p = {}
 
-
-print("[green]Welcome to Linux Pinger![/green]")
-print("[cyan]Please enter the network you wish to test...[/cyan]")
-print("Example: < 192.168.10.0/24 >")
-subnet = input("\nEnter network: ")
-print("\n")
+rprint("[green]Welcome to Linuxpinger![/green]")
+rprint("[cyan]Please enter the network you wish to test...[/cyan]")
+rprint("Example: <192.168.10.0/24>")
+subnet = input("Enter network: ")
 network = ipaddress.ip_network(subnet)
 
-for n in network.hosts():
+hosts = network.hosts()
+for n in hosts:
     IP = str(n)
-    p[IP] = Popen(['ping', '-c', '4', '-i', '0.2', IP], stdout=DEVNULL)
+    ping[IP] = Popen(['ping', '-c', '4', '-i', '0.2', IP], stdout=DEVNULL)
 
-while p:
-    for IP, proc in p.items():
-        if proc.poll() is not None:
-            del p[IP]
-            if proc.returncode == 0:
+while ping:
+    for IP, process in ping.items():
+        if process.poll() is not None:
+            del ping[IP]
+            if process.returncode == 0:
                 active_list.append(IP)
-            elif proc.returncode == 1:
+            elif process.returncode == 1:
                 inactive_list.append(IP)
             else:
                 print(f"{IP} ERROR")
@@ -41,7 +37,7 @@ while p:
 table = Table(title="PING REPORT \n" + localtime)
 table.add_column("Active Hosts", justify="center", style="green")
 table.add_column("Inactive Hosts", justify="center", style="red")
-for (a, i) in itertools.zip_longest(active_list, inactive_list):
+for (a,i) in itertools.zip_longest(active_list, inactive_list):
     table.add_row(a, i)
 console = Console()
 console.print(table)
