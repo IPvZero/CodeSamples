cf1e5ef3de47458a377cadd7c4a956e16523d740
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-03-09 02:58:18 +0000

    Create easypeasy.py

---
+++ ADV-NET/pyATS/easypeasy.py
@@ -0,0 +1,16 @@
+from pyats import aetest
+from genie.conf import Genie
+
+class DeviceTestcase(aetest.Testcase):
+    @aetest.setup
+    def setup(self):
+        self.parameters["testbed"].connect(log_stdout=False)
+
+    @aetest.test
+    def show_version(self):
+        for device in self.parameters["testbed"].devices.values():
+            show_ver = device.parse("show version")
+            assert show_ver["version"]["version_short"] == "15.1"
+
+topology = Genie.init("testbed.yaml")
+aetest.main(testbed=topology)
