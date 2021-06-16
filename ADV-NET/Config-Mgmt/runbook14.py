8cbbe9c40a3d63e4b7ba6fbb78f9e9329244bf26
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-06-16 13:41:14 +0100

    Create runbook14.py

---
+++ ADV-NET/Config-Mgmt/runbook14.py
@@ -0,0 +1,11 @@
+from nornir import InitNornir
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+def pull_random_info(task):
+    task.run(task=napalm_get, getters=["get_facts", "get_environment"])
+
+results = nr.run(task=pull_random_info)
+print_result(results)
