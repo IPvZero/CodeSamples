ae1df7c284988a7cd7744d18eea3dc557d78a71c
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-06-17 15:49:50 +0100

    Create runbook1.py

---
+++ ADV-NET/Security/runbook1.py
@@ -0,0 +1,14 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+commands = input("\nEnter Commands You Wish To Send: ")
+cmds = commands.split(",")
+
+def push_show_commands(task):
+    for cmd in cmds:
+        task.run(task=send_command, command=cmd)
+
+results = nr.run(task=push_show_commands)
