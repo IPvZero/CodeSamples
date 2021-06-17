471c9b42dc887dfaafc36f7e2dfc7e1d008ace05
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-06-17 15:50:08 +0100

    Create runbook2.py

---
+++ ADV-NET/Security/runbook2.py
@@ -0,0 +1,11 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_configs_from_file
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+
+def push_configs(task):
+    task.run(task=send_configs_from_file, file="testconfigs.txt")
+
+results = nr.run(task=push_configs)
