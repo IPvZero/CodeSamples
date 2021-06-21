0cef09cb1a76def6baa75426ec6df90bd96b8ab2
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-06-22 00:32:37 +0100

    Create runbook5.py

---
+++ ADV-NET/Credentials/runbook5.py
@@ -0,0 +1,16 @@
+import os
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+nr.inventory.defaults.username = os.environ["DEFAULT_USERNAME"]
+nr.inventory.defaults.password = os.environ["DEFAULT_PASSWORD"]
+
+
+def credential_test(task):
+    task.run(task=send_command, command="show ip interface brief")
+
+results = nr.run(task=credential_test)
+print_result(results)
