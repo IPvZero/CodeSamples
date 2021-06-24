be9f1668671fda4913c1e251a95461c5fce06132
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-06-24 18:48:04 +0100

    Update script4.py

--- ADV-NET/IP-Reachability/script4.py
+++ ADV-NET/IP-Reachability/script4.py
@@ -11,20 +11,24 @@ from rich import print
 from rich.console import Console
 from rich.table import Table
 
+ping = {}
+active_list = []
+inactive_list = []
+
 CLEAR = "clear"
 os.system(CLEAR)
 localtime = time.asctime(time.localtime(time.time()))
 
-rprint("[green]Welcome to Linuxpinger![/green]")
-rprint("[cyan]Please enter the network you wish to test...[/cyan]")
-rprint("Example: <192.168.10.0/24>")
+print("[green]Welcome to Linuxpinger![/green]")
+print("[cyan]Please enter the network you wish to test...[/cyan]")
+print("Example: <192.168.10.0/24>")
 subnet = input("Enter network: ")
 network = ipaddress.ip_network(subnet)
 
 hosts = network.hosts()
 for n in hosts:
     IP = str(n)
-    ping[IP] = Popen(['ping', '-c', '4', '-i', '0.2', IP], stdout=DEVNULL)
+    ping[IP] = Popen(["ping", "-c", "4", "-i", "0.2", IP], stdout=DEVNULL)
 
 while ping:
     for IP, process in ping.items():
@@ -41,7 +45,7 @@ while ping:
 table = Table(title="PING REPORT \n" + localtime)
 table.add_column("Active Hosts", justify="center", style="green")
 table.add_column("Inactive Hosts", justify="center", style="red")
-for (a,i) in itertools.zip_longest(active_list, inactive_list):
+for (a, i) in itertools.zip_longest(active_list, inactive_list):
     table.add_row(a, i)
 console = Console()
 console.print(table)
