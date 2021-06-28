243f3f3a244026318d8bf6e9225a5d7f8f261c29
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-06-28 21:15:44 +0100

    Update runbook4.py

--- ADV-NET/Templating/runbook4.py
+++ ADV-NET/Templating/runbook4.py
@@ -10,8 +10,8 @@ def test_templates(task):
     template = task.run(
         task=template_file, template="snmp.j2", path=f"{task.host.platform}-templates"
     )
-    task.host["ospf_config"] = template.result
-    rendered = task.host["ospf_config"]
+    task.host["snmp_config"] = template.result
+    rendered = task.host["snmp_config"]
     configuration = rendered.splitlines()
     task.run(task=send_configs, configs=configuration)
 
