42fee3a82ccd619e3115d7c9c03015b3085bf659
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-07-19 17:24:37 +0100

    Create example1.py

---
+++ Intro_Python_Net_Eng/control-flow/example1.py
@@ -0,0 +1,10 @@
+vendor = input("Enter the vendor you wish to automate? ")
+
+if vendor == "cisco":
+    print("You have selected Cisco")
+
+elif vendor == "aruba":
+    print("You have selected Aruba")
+
+else:
+    print("Unrecognised vendor")
