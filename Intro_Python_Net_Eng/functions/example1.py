37c4a777886bf5795d562572434dadb79d83a5a0
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-08-02 23:39:31 +0100

    Create example1.py

---
+++ Intro_Python_Net_Eng/functions/example1.py
@@ -0,0 +1,7 @@
+def warning_text():
+    print("This network belongs to CBT NUGGETS - don't mess around with it")
+
+warning_text()
+print("blah blah blah")
+print("more blah blah blah")
+warning_text()
