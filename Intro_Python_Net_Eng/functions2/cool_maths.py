1d1f196c2ad903bf2b5155b6354340208a62d5c8
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-08-05 21:47:20 +0100

    Create cool_maths.py

---
+++ Intro_Python_Net_Eng/functions2/cool_maths.py
@@ -0,0 +1,7 @@
+def added_and_multiplied_and_divided(num1, num2):
+    added_total = num1 + num2
+    multiplied_total = num1 * num2
+    divided_total = num1 / num2
+
+    return added_total, multiplied_total, divided_total
+  
