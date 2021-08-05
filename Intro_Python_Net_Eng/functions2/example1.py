0348a6d5345f933923ec7cebe7628d53afcd11ca
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-08-05 21:44:32 +0100

    Create example1.py

---
+++ Intro_Python_Net_Eng/functions2/example1.py
@@ -0,0 +1,17 @@
+def my_adder_function(my_list):
+    total = 0
+    for num in my_list:
+        total = total + num
+    
+    print(total)
+    
+my_adder_function(1,3,5)
+
+
+#def my_better_adder_function(*args):
+ #   total = 0
+  #  for n in args:
+   #     total = total + n
+   # print(total)
+
+#my_better_adder_function(1,3,5)
