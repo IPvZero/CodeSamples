2ea05c739e3987791c230a215f2d12ea7f49b8c0
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-08-05 21:46:33 +0100

    Create example7.py

---
+++ Intro_Python_Net_Eng/functions2/example7.py
@@ -0,0 +1,8 @@
+from cool_maths import added_and_multiplied_and_divided
+
+
+x,y,z = added_and_multiplied_and_divided(10, 5)
+
+print(f"The added total is {x}")
+print(f"The multiplied total is {y}")
+print(f"The divided total is {z}")
