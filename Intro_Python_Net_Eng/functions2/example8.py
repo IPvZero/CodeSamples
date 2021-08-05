005becdce20adf876539ec3fa27a9c3117c151a1
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-08-05 21:46:57 +0100

    Create example8.py

---
+++ Intro_Python_Net_Eng/functions2/example8.py
@@ -0,0 +1,19 @@
+#name = "John"
+
+#def print_name():
+#    name = "Trevor"
+#    def inner_func():
+#        print(name)
+#    inner_func()
+
+
+#print(name)
+#print_name()
+
+
+device = "R1"
+
+def print_my_device():
+    print(device)
+
+print_my_device()
