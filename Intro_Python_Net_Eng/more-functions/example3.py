862862bd7052795f65fee11b736209b3f4b8eab2
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-08-12 22:10:50 +0100

    Create example3.py

---
+++ Intro_Python_Net_Eng/more-functions/example3.py
@@ -0,0 +1,12 @@
+#def regular_function(num1, num2):
+#    """
+#    Simple example of a regular function
+#    """
+#    result = num1 + num2
+#    return result
+
+#answer = regular_function(1,3)
+#print(answer)
+
+adder = lambda num1, num2: num1 + num2
+print(adder(23,81))
