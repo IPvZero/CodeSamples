eeb6eaf3cd94597f91a038bc6d2daaeefab2e6ee
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-08-18 00:19:29 +0100

    Create example1.py

---
+++ Intro_Python_Net_Eng/libraries/example1.py
@@ -0,0 +1,14 @@
+from scrapli.driver.core import IOSXEDriver
+
+MY_DEVICE = {
+    "host": "sandbox-iosxe-latest-1.cisco.com",
+    "auth_username": "developer",
+    "auth_password": "C1sco12345",
+    "auth_strict_key": False,
+}
+
+conn = IOSXEDriver(**MY_DEVICE)
+conn.open()
+response = conn.send_command("show ip interface brief")
+print(response.result)
+conn.close()
