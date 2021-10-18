00a0fe069f55bda066cb9ca3e5b13c4668031ad7
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-10-18 23:55:06 +0100

    Create multiprocess2.py

---
+++ Intro_Python_Net_Eng/concurrency/multiprocess2.py
@@ -0,0 +1,30 @@
+from time import perf_counter
+from concurrent.futures import ProcessPoolExecutor
+from scrapli.driver.core import IOSXEDriver
+from inv import DEVICES
+
+start = perf_counter()
+
+
+def send_cmd(device):
+    with IOSXEDriver(
+        host=device["host"],
+        auth_username="john",
+        auth_password="cisco",
+        auth_strict_key=False,
+        ssh_config_file=True,
+    ) as conn:
+        response = conn.send_command("show version")
+        return response.result
+
+
+if __name__ == "__main__":
+    with ProcessPoolExecutor() as executor:
+        results = executor.map(send_cmd, DEVICES)
+
+    for result in results:
+        print(result)
+
+    end = perf_counter()
+    total_time = end - start
+    print(total_time)
